d826ba8285d19eebef381f76d104621113315a6f
michaelrzg <michaelrizig@gmail.com>
2024-10-11 12:10:19 -0400

    taking a different apprach

--- LSTM.py
+++ LSTM.py
@@ -1,73 +1,46 @@
 import numpy as np
-class LSTM:
-    def __init__(self,itterations) -> None:
-        self.st_weights = None
-        self.input_weights = None
-        self.bias = None
-        self.short_term=0
-        self.long_term=0
-        self.itterations = itterations
-        # initilize weights and bias
-        self.st_weights= [np.random.random() for x in range(4)]
-        self.input_weights=  [np.random.random() for x in range(4)]
-        self.bias=  [np.random.random() for x in range(4)]
-    
-    # sigmoid activation funciton
-    def sigmoid(self,data):
-        return 1/(1 + np.exp(-data))
-    
-    #tanh activation function
-    def tanh(self,data):
-        return (np.exp(data)-np.exp(-data))/(np.exp(data)+np.exp(-data))
 
-    def forget_gate(self,data):
-        # short term mempry * short term memeory weight + input * input weight + bias for cell
-        summation = np.dot([self.short_term,data],[self.st_weights[0],self.input_weights[0]]) + self.bias[0]
-        # activation function
-        output = self.sigmoid(summation)
-        # determine % of long term to remember or 'forget'
-        self.long_term = self.long_term*output
-        return self.long_term
-    
-    
-    def input_gate(self,data):
-        # % to remember from current long term memory
-        # short term mempry * short term memeory weight + input * input weight + bias for cell
-        summation = ((data * self.input_weights[1]) + self.short_term*self.st_weights[1]) + self.bias[1]
-        # activation function
-        remember = self.sigmoid(summation)
-        
-        # potential memory 
-        # short term mempry * short term memeory weight + input * input weight + bias for cell
-        summation2 = ((data * self.input_weights[2]) + self.short_term*self.st_weights[2]) + self.bias[2]
-        # activation function
-        potential = self.tanh(summation2)
+class LSTM():
+    def __init__(self, n_states, n_features) -> None:
+        # num of n_states
+        self.n_states = n_states
+        # learnables
+        # forget gate values
+        self.Uf = 0.1*np.random.rand(n_states,n_features)
+        self.bf = 0.1*np.random.rand(n_states,n_features)
+        self.wf = 0.1*np.random.rand(n_states,n_states)
 
-        self.long_term = self.long_term + (remember*potential)
+        # input gate values
+        self.Ui = 0.1*np.random.rand(n_states,n_features)
+        self.bi = 0.1*np.random.rand(n_states,n_features)
+        self.wi = 0.1*np.random.rand(n_states,n_states)
 
-        return self.long_term
+        #output gate values
+        self.Uo = 0.1*np.random.rand(n_states,n_features)
+        self.bo = 0.1*np.random.rand(n_states,n_features)
+        self.wo = 0.1*np.random.rand(n_states,n_states)
 
-    def update_short_term(self,data):
-        # short term mempry * short term memeory weight + input * input weight + bias for cell
-        summation = np.dot([self.short_term,data],[self.st_weights[3],self.input_weights[3]]) + self.bias[3]
-        # activation function
-        remember = self.sigmoid(summation)
-        # next get longterm memory contribution
-        long_term_contribution = self.tanh(self.long_term)
-        #update short term for next run
-        self.short_term = remember*long_term_contribution
-        return self.short_term
-    
-    def fit(self,data):
-        for i in range(self.itterations):
-            for d in data:
-                #forget gate
-                self.forget_gate(d)
-                #input gate
-                self.input_gate(d)
-                #update short term
-                self.update_short_term(d)
-        print("Long term: ",self.long_term, " Short term: ", self.short_term)
+        #output gate values
+        self.Uo = 0.1*np.random.rand(n_states,n_features)
+        self.bo = 0.1*np.random.rand(n_states,n_features)
+        self.wo = 0.1*np.random.rand(n_states,n_states)
 
-lstm = LSTM(4)
-lstm.fit([0,.5,.25])
+        # c tilde
+        self.Ug = 0.1*np.random.rand(n_states,n_features)
+        self.bg = 0.1*np.random.rand(n_states,n_features)
+        self.wg = 0.1*np.random.rand(n_states,n_states)
+
+    def forward_pass(self,data):
+        # find vector length
+        self.max_vector = max(data.shape)
+        # save data in class
+        self.data = data
+        # set up lists to store for short term, long term, and c_tilde (last gate) states at points 0-(t-1)
+        self.short_term = [np.zeros((self.n_states,1)) for x in range(self.max_vector+1)]
+        self.long_term = [np.zeros((self.n_states,1)) for x in range(self.max_vector+1)]
+        self.c_tilde = [np.zeros((self.n_states,1)) for x in range(self.max_vector)]
+
+        # set up lists to store gate inner values at points 0-(t-1) (for debugging later)
+        self.forget_gate = [np.zeros((self.n_states,1)) for x in range(self.max_vector)]
+        self.input_gate = [np.zeros((self.n_states,1)) for x in range(self.max_vector)]
+        self.output_gate = [np.zeros((self.n_states,1)) for x in range(self.max_vector)]    
